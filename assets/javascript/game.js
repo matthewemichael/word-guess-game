efb9808cb7e59a3efadf36bbfe29babbc347921f
Matt Michael <matthewemichael@gmail.com>
2019-08-10 17:14:01 -0500

    fixed end of game loop bug, created initialize game function, cleaned up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ var possibleWords = [
   "GRAND CANYON", 
   "ROCKY MOUNTAIN", 
   "ZION", 
-  "YELLOWSTONE", 
+  "YELLOWSTONE",
   "YOSEMITE", 
   "GRAND TETON", 
   "GLACIER", 
@@ -62,19 +62,107 @@ var possibleWords = [
   "HALEAKALA"
 ];
 
-var maxTries = 7;
 var guessedLetters = [];
 var guessingWord = [];
 var usedGuessingwWords = [];
 var wordToMatch;
 var numGuess;
 var wins = 0;
-var pause = false; // This var and setTimout funtion to not listen for keypress while game resets
+var pause = false; // This var and setTimout function to not listen for keypress while game resets
 var loseSound = new Audio("./assets/sounds/ahahah.mp3");
 var winSound = new Audio("./assets/sounds/clever.wav");
+var championSound = new Audio("./assets/sounds/crazysob.mp3");
 
 //Starts game
-resetGame()
+function initializeGame() {
+
+  // Get a new word
+  wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase();
+  // Set number of guesses (higher or lower) based on word length
+  if (wordToMatch.length <= 4) {
+    numGuess = 4
+  } else if (wordToMatch.length >4 && wordToMatch.length <= 7) {
+    numGuess = Math.floor(wordToMatch.length * .67)
+  } else if (wordToMatch.length >7 && wordToMatch.length <= 10) {
+    numGuess = Math.floor(wordToMatch.length * .5)
+  } else if (wordToMatch.length >10 && wordToMatch.length <= 14) {
+    numGuess = Math.floor(wordToMatch.length * .52)
+  } else if (wordToMatch.length >14) {
+    numGuess = 7;
+  }
+
+  // Get underscores for guessingWord from wordToMatch
+  for (var i=0; i < wordToMatch.length; i++){
+    // Put a space instead of an underscore between multi-word options in possibleWords array
+    if (wordToMatch[i] === " ") {
+      guessingWord.push(" ")
+    } 
+    else {
+      guessingWord.push("_");
+    }
+  }
+  updateDisplay();
+};
+
+//Reset the game
+function resetGame() {
+  if (usedGuessingwWords.length === possibleWords.length) {
+    championSound.play() // Toggle line comment on for almost the entire possibleWords array to hear this end of game sound clip
+    usedGuessingwWords = []
+    wins = 0
+    setTimeout(resetGame, 6000); // Note for future change - shorten possibleWords, make jumbotron display congratulatory message upon guessing all possibilites
+  }
+  else {
+    pause = false;
+    // Restores blinking "...get started" message
+    document.getElementById('welcome').className = 'blink';
+    
+    // Get a new word
+    wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase();
+    console.log(wordToMatch)
+    // If new word has already been used randomly select another
+    if (usedGuessingwWords.includes(wordToMatch) === true) {
+      resetGame();
+    }
+    
+    // Set number of guesses (higher or lower) based on word length
+    if (wordToMatch.length <= 4) {
+      numGuess = 4
+    } else if (wordToMatch.length >4 && wordToMatch.length <= 7) {
+      numGuess = Math.floor(wordToMatch.length * .67)
+    } else if (wordToMatch.length >7 && wordToMatch.length <= 10) {
+      numGuess = Math.floor(wordToMatch.length * .5)
+    } else if (wordToMatch.length >10 && wordToMatch.length <= 14) {
+      numGuess = Math.floor(wordToMatch.length * .52)
+    } else if (wordToMatch.length >14) {
+      numGuess = 7;
+    }
+
+    // Reset word arrays
+    guessedLetters = [];
+    guessingWord = [];
+
+    // Reset the guessed word
+    for (var i=0; i < wordToMatch.length; i++){
+      // Put a space instead of an underscore between multi-word options in possibleWords array
+      if (wordToMatch[i] === " ") {
+        guessingWord.push(" ")
+      } 
+      else {
+        guessingWord.push("_");
+      }
+    }
+    updateDisplay();
+  }
+};
+
+// Update the Display
+function updateDisplay () {
+  document.getElementById("totalWins").innerText = wins;
+  document.getElementById("currentWord").innerText = guessingWord.join("");
+  document.getElementById("remainingGuesses").innerText = numGuess;
+  document.getElementById("guessedLetters").innerText =  guessedLetters.join(" ");
+};
 
 // Wait for key press
 document.onkeydown = function(event) {
@@ -82,17 +170,15 @@ document.onkeydown = function(event) {
   if (isLetter(event.key) && pause === false) {
   checkForLetter(event.key.toUpperCase());
   }
-  // Turn of blinking "...get started" message on keypress
+  // Turn off blinking "...get started" message on keypress
   document.getElementById('welcome').className = 'noBlink';
-}
+};
 
 // Check if key pressed is between A-Z or a-z
-
 var isLetter = function(ch){
   return typeof ch === "string" && ch.length === 1
   && (ch >= "a" && ch <= "z" || ch >= "A" && ch <= "Z");
-}
-
+};
 
 // Check if letter is in word
 function checkForLetter(letter) {
@@ -108,7 +194,8 @@ function checkForLetter(letter) {
         // Increment # of wins and add word to usedGuessingWords
         wins++
         // Add word to usedGuessingWords array to not be repeated
-        usedGuessingwWords.push(wordToMatch);
+        usedGuessingwWords.push(wordToMatch)
+        console.log(usedGuessingwWords)
         pause = true;
         winSound.play();
         updateDisplay();
@@ -127,6 +214,7 @@ function checkForLetter(letter) {
     if (numGuess === 0) {
       // Add word to usedGuessingWords array to not be repeated
       usedGuessingwWords.push(wordToMatch);
+      console.log(usedGuessingwWords)
       // Display word before reseting game
       guessingWord = wordToMatch.split();
       pause = true;
@@ -135,56 +223,6 @@ function checkForLetter(letter) {
     }
   }
   updateDisplay();
-}
-
-//Reset the game
-function resetGame() {
-  numGuess = maxTries;
-  pause = false;
-  // Restores blinking "...get started" message
-  document.getElementById('welcome').className = 'blink';
-  // Get a new word
-  wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase();
-
-  // If new word has already been used randomly select another - !!freaks out after all options have been played!!
-  if (usedGuessingwWords.includes(wordToMatch) === true && (usedGuessingwWords !== possibleWords))  {
-    resetGame();
-  }
-
-  // Set number of guesses (higher or lower) based on word length
-  if (wordToMatch.length <= 4) {
-    numGuess = Math.floor(wordToMatch.length * .75)
-  } else if (wordToMatch.length >4 && wordToMatch.length <= 7) {
-    numGuess = Math.floor(wordToMatch.length * .67)
-  } else if (wordToMatch.length >7 && wordToMatch.length <= 10) {
-    numGuess = Math.floor(wordToMatch.length * .5)
-  } else if (wordToMatch.length >10 && wordToMatch.length <= 14) {
-    numGuess = Math.floor(wordToMatch.length * .52)
-  } else if (wordToMatch.length >14) {
-    numGuess = 7;
-  }
-
-  // Reset word arrays
-  guessedLetters = [];
-  guessingWord = [];
-
-  // Reset the guessed word
-  for (var i=0; i < wordToMatch.length; i++){
-    // Put a space instead of an underscore between multi-word options in possibleWords array
-    if (wordToMatch[i] === " ") {
-      guessingWord.push(" ")
-    } 
-    else {
-      guessingWord.push("_");
-    }
-  }
-  updateDisplay();
-}
-
-// Update the Display
-function updateDisplay () {
-  document.getElementById("totalWins").innerText = wins;
-  document.getElementById("currentWord").innerText = guessingWord.join("");
-  document.getElementById("remainingGuesses").innerText = numGuess;
-  document.getElementById("guessedLetters").innerText =  guessedLetters.join(" ");
 };
+
+initializeGame();
